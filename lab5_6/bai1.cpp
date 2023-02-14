9a9d66ac87e07dfa05a623aca14e6170ae03036d
Quyen Le <quyenlqvpd07814@fpt.edu.vn>
2023-02-14 18:55:30 +0700

    update

--- lab5_6/bai1.cpp
+++ lab5_6/bai1.cpp
@@ -1,14 +1,4 @@
 
-/*  BÀI 1: Viết sử dụng switch case.
-    Nhập tên học sinh;
-    Nhập điểm trung bình Toán, Lý, Hóa của học sinh đó.
-    Tính điểm trung bình 3 môn Toán, Lý, Hóa và
-    Kiểm tra xem học sinh đó xếp hạng loại gì.
-    Biết:   9-10 điểm => xếp loại A
-            7-8 điểm => xếp loại B
-            5-6 điểm => xếp loại C
-            Dưới 5 điểm => xếp loại D
-*/
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
